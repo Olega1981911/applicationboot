0029c5454d1829ad9139c2c9f8c66e36b45113b9
Olega1981911 <olegchem97@gmail.com>
2023-06-07 23:25:39 +0300

    repair example

--- src/main/java/com/application/applicationboot/repositories/UserRepository.java
+++ src/main/java/com/application/applicationboot/repositories/UserRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    public User getUserByUsername(String username);
+    User getUserByUsername(String username);
 
 }
